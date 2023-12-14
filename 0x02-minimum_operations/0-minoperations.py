d0f815936fb8c69e038636baac3638bce9ec0e3b
CalvinKarume <johncalvinkarume@gmail.com>
2023-12-14 07:06:32 -0800

    DONE TASKS

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,16 +3,18 @@
 
 
 def minOperations(n):
-	"""
-	Given a text file with a single 'H,'
-	 the method 'minOperations' calculates the fewest
-	 operations needed (Copy All and Paste) to achieve
-	 exactly n 'H' characters. Returns 0 if impossible
-	"""
+    """
+    Given a text file with a single 'H,'
+    the method 'minOperations' calculates the fewest
+    operations needed (Copy All and Paste) to achieve
+    exactly n 'H' characters. Returns 0 if impossible.
+    """
     if n <= 1:
         return 0
 
     for x in range(2, n + 1):
         if n % x == 0:
-            return minOperations(int(n / x)) + x
+            return minOperations(n // x) + x
+
     return n
+
