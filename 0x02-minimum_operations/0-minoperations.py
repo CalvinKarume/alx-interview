bd5c943340568e963ef5a236328be303fbf1c62d
CalvinKarume <johncalvinkarume@gmail.com>
2023-12-14 07:02:35 -0800

    NIICCEEE

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,35 +9,10 @@ def minOperations(n):
 	 operations needed (Copy All and Paste) to achieve
 	 exactly n 'H' characters. Returns 0 if impossible
 	"""
-pasted_chars = 1
-clipboard = 0
-counter = 0
-
-while pasted_chars < n:
-    if clipboard == 0:
-        clipboard = pasted_chars
-        counter += 1
-
-    if pasted_chars == 1:
-        pasted_chars += clipboard
-        counter += 1
-        continue
-
-    remaining = n - pasted_chars
-
-    if remaining < clipboard:
+    if n <= 1:
         return 0
 
-    if remaining % pasted_chars != 0:
-        pasted_chars += clipboard
-        counter += 1
-    else:
-        clipboard = pasted_chars
-        pasted_chars += clipboard
-        counter += 2
-
-if pasted_chars == n:
-    return counter
-else:
-    return 0
-
+    for x in range(2, n + 1):
+        if n % x == 0:
+            return minOperations(int(n / x)) + x
+    return n
