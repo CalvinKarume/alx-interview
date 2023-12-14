a80f9f07d3c1d593cf7112a90a6978ff5c2b174e
CalvinKarume <johncalvinkarume@gmail.com>
2023-12-14 06:39:33 -0800

    Interestingggg

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+	"""
+	Given a text file with a single 'H,'
+	 the method 'minOperations' calculates the fewest
+	 operations needed (Copy All and Paste) to achieve
+	 exactly n 'H' characters. Returns 0 if impossible
+	"""
+    if n <= 1:
+        return 0
+
+    for x in range(2, n + 1):
+        if n % x == 0:
+            return minOperations(int(n / x)) + x
+    return n
