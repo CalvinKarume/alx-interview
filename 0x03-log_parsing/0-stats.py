b28e8fda96a9acd491034a0233691555d6875fee
CalvinKarume <johncalvinkarume@gmail.com>
2023-12-21 11:59:13 -0800

    DONENDONE

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,35 @@
 #!/usr/bin/python3
-"""A script that reads stdin line by line and compuutes metrics"""
-import sys
+"""A script that reads stdin line by line and computes metrics"""
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache:
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    def print_statistics():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+            print()
 
-    def parse_line(line):
-        """ Checks the line for matches """
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            file_size[0] += int(word[-1])
-            status_code = int(word[-2])
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
+except KeyboardInterrupt:
+    pass
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
 
-    linenum = 1
-    try:
-        for line in sys.stdin:
-            parse_line(line)
-            """ after every 10 lines or keyboard interruption,
-		 print the stats from the beginning """
-            if linenum % 10 == 0:
-                print_statistics()
-            linenum += 1
-    except KeyboardInterrupt:
-        print_statistics()
-        raise
-    print_statistics()
