12108c4b301b840500768b6485395cd26ae16a39
CalvinKarume <johncalvinkarume@gmail.com>
2024-01-31 11:25:28 -0800

    DoNE N DoNE

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """Changes come from within"""
 
-
 def rotate_2d_matrix(matrix):
     """makeChange computes the minimum coins needed to reach
      a target total using a given set of coin values.."""
-    n = len(matrix)
+    left, right = 0, len(matrix) - 1
 
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # Reverse each row
-    for i in range(n):
-        matrix[i].reverse()
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
