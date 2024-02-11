2e4768f1dadef0d766370d93cea4bc50e245de5b
CalvinKarume <Johncalvinkarume@gmail.com>
2024-02-11 22:17:28 +0300

    ISLaND PERImETeR

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,16 +12,16 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                # Check left neighbor
+                # Check left
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                # Check right neighbor
+                # Check right
                 if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-                # Check top neighbor
+                # Check top
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                # Check bottom neighbor
+                # Check bottom
                 if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
 
