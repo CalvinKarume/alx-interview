a04ddf4747f97620c123c14fdc19c1c73bf11d7c
CalvinKarume <Johncalvinkarume@gmail.com>
2024-02-14 23:17:06 +0300

    Prime Game DOne

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-""" Prime Game """
+"""Prime game module."""
 
-def isWinner(m, nums):
-    if not nums or m < 1:
+def isWinner(x, nums):
+    """Determines the winner of a prime game session"""
+
+    if x < 1 or not nums:
         return None
-
-    max_num = max(nums)
-    sieve = [False] * (max_num + 1)
-    primes_count = 0
-
-    for i in range(2, int(max_num ** 0.5) + 1):
-        if not sieve[i]:
-            for j in range(i * i, max_num + 1, i):
-                sieve[j] = True
-
-    for i in range(2, len(sieve)):
-        if not sieve[i]:
-            primes_count += 1
-
-    maria_primes = sum(1 for n in nums if not sieve[n])
-    ben_primes = len(nums) - maria_primes
-
-    if maria_primes > ben_primes:
-        return "Maria"
-    elif maria_primes < ben_primes:
-        return "Ben"
-    else:
+    marias_wins, bens_wins = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
         return None
-
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
